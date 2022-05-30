38dfbc02fffb2e7881c128e76798bbdd730c2369
MarcoTenezaca001 <tnzmarko70@gmail.com>
2022-05-30 12:32:02 -0500

    prueba1

---
+++ app/src/main/java/fisei/uta/edu/ec/practicapareja/MainActivity2.java
@@ -0,0 +1,14 @@
+package fisei.uta.edu.ec.practicapareja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
